67def3c9a632850fb09a00169d2d5c796971ddbb
responsecode502 <responsecode502@gmail.com>
2026-06-08 21:25:06 +0300

    idk

--- btrfs/deploy.sh
+++ btrfs/deploy.sh
@@ -35,28 +35,35 @@ xbps-install -u xbps --yes
 xbps-install -S --yes
 
 echo "=== 6 - fstab ==="
-# Используем хередок (cat << 'EOF'), чтобы безопасно записать многострочный текст
-cat << 'EOF' > /mnt/etc/fstab
+# Убираем кавычки вокруг EOF, чтобы переменные UUID заменились на реальные значения
+cat << EOF > /mnt/etc/fstab
 UUID=$UUID_BTRFS    /            btrfs    rw,noatime,compress=zstd:1,ssd,subvol=@        0 0
 UUID=$UUID_BTRFS    /home        btrfs    rw,noatime,compress=zstd:1,ssd,subvol=@home    0 0
 UUID=$UUID_EFI      /boot/efi    vfat     rw,noatime,fmask=0077,dmask=0077               0 2
 EOF
 
+
 echo "=== 7 - mounts, dirs, chroot ==="
 
+# Монтируем системные псевдофайловые системы
 for dir in dev proc sys run; do
     mount --mkdir --bind /$dir /mnt/$dir
 done
-
 mount --mkdir --bind /sys/firmware/efi/efivars /mnt/sys/firmware/efi/efivars
 
-mount -a
-
+# Выполняем команды внутри chroot
 chroot /mnt /bin/bash << 'CHROOT_EOF'
+# Настройка GRUB для BTRFS
 echo "GRUB_BTRFS_SUBVOLROOT=@" >> /etc/default/grub
-echo "GRUB_DISABLE_OS_PROBEL=false" >> /etc/default/grub
-grub-install /dev/nvme0n1
+
+# Исправляем опечатку в DISABLE_OS_PROBER (в Void Linux по умолчанию он и так выключен)
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub 
+
+# Правильная установка для UEFI
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=void_grub --recheck
+
+# КРИТИЧЕСКИЙ ШАГ: Создаем актуальное меню загрузки GRUB с новыми путями к ядру
+grub-mkconfig -o /boot/grub/grub.cfg
+
 exit
 CHROOT_EOF
-
-echo "=== Script end ==="
