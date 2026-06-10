9f3a1bef81bcf7c79328c3288ef52df9a8585553
responsecode502 <responsecode502@gmail.com>
2026-06-10 15:54:17 +0300

    2 scripts now

---
+++ btrfs_snapper/2_snapper.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Перенос снэпшотов в бэкап-папку.
+umount /.snapshots || true
+mv /.snapshots /.snapshots-backup
+
+# Установка пакетов, запуск сервисов
+xbps-install -S snapper dcron grub-btrfs --yes
+ln -sf /etc/sv/dcron /var/service/
+
+snapper -c root create-config /
+rmdir /.snapshots
+mv /.snapshots-backup /.snapshots
+mount -a
+snapper -c root create -d "Pure system"
+grub-mkconfig -o /boot/grub/grub.cfg
